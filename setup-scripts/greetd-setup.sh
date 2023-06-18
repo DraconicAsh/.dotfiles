6c1b2d93540f38635b047e2784a70a9803fac643
DraconicAsh <85758158+ConfidentlyAsh@users.noreply.github.com>
2023-06-18 02:47:14 -0500

    Only hard link config.toml and gtkgreet.css

--- setup-scripts/greetd-setup.sh
+++ setup-scripts/greetd-setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Ensure clean /etc/greetd directory
+US=$USER
+
 sudo -s -- <<EOF
+    # Ensure clean /etc/greetd directory
     rm -r /etc/greetd
     mkdir /etc/greetd
+    
+    # Hard link config files to /etc/greetd
+    # Hard link is used so that user "greeter" can read files
+    ln /home/$US/.dotfiles/etc/greetd/config.toml /etc/greetd
+    ln /home/$US/.dotfiles/etc/greetd/gtkgreet.css /etc/greetd
+    
+    # Copy background and environment file to /etc/greetd directory
+    cp /home/$US/.dotfiles/etc/greetd/bg.jpg /etc/greetd
+    cp /home/$US/.dotfiles/etc/greetd/environments /etc/greetd
 EOF
-
-# Hard link config files to /etc/greetd
-# Hard link is used so that user "greeter" can read files
-for file in ~/.dotfiles/etc/greetd/*; do
-	if [ -f "$file" ]; then
-	sudo ln $file /etc/greetd
-	fi
-done
-
-# Copy background to /etc/greetd directory
-sudo cp ~/.dotfiles/etc/greetd/backgrounds/bg.jpg /etc/greetd

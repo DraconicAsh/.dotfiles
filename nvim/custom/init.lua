e4ed46798c1c9448af59b4f323c43c978475e2af
DraconicAsh <85758158+ConfidentlyAsh@users.noreply.github.com>
2023-06-23 20:09:12 -0500

    Change shiftwidth to 4 & run on relative numbers

---
+++ nvim/custom/init.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt
+
+opt.shiftwidth = 4
+
+opt.relativenumber = true
--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -1,22 +1,22 @@
 local plugins = {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "rust",
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {
+            "bash",
+            "rust",
+            "json",
 
-      -- git
-      "git_config",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
+            -- git
+            "git_config",
+            "git_rebase",
+            "gitattributes",
+            "gitcommit",
+            "gitignore",
 
-      -- defaults
-      "vim",
-      "lua"
+            -- defaults
+            "vim",
+            "lua"
+        }
     }
-  }
 }
-
 return plugins

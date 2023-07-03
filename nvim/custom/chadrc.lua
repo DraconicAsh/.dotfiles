1ec05200dc1701f6a7b82736e9d9c3cb32db75c1
DraconicAsh <85758158+ConfidentlyAsh@users.noreply.github.com>
2023-07-02 22:33:55 -0500

    Make neovim transparent

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -1,5 +1,12 @@
 ---@type ChadrcConfig 
 local M = {}
-M.ui = {theme = 'nightowl'}
+M.ui = {
+    theme = 'nightowl',
+    transparency = true,
+    statusline = {
+        theme = 'minimal',
+        separator_style = 'round'
+    }
+}
 M.plugins = "custom.plugins"
 return M

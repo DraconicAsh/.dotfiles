c750d24de93cefd80a9945cba7835ca72418e6d5
DraconicAsh <85758158+ConfidentlyAsh@users.noreply.github.com>
2023-07-25 19:22:30 -0500

    Switch greeter to tuigreet

--- setup-scripts/greetd-setup.sh
+++ setup-scripts/greetd-setup.sh
@@ -10,9 +10,4 @@ sudo -s -- <<EOF
     # Hard link config files to /etc/greetd
     # Hard link is used so that user "greeter" can read files
     ln /home/$US/.dotfiles/etc/greetd/config.toml /etc/greetd
-    ln /home/$US/.dotfiles/etc/greetd/gtkgreet.css /etc/greetd
-    
-    # Copy background and environment file to /etc/greetd directory
-    cp /home/$US/.dotfiles/etc/greetd/bg.jpg /etc/greetd
-    cp /home/$US/.dotfiles/etc/greetd/environments /etc/greetd
 EOF

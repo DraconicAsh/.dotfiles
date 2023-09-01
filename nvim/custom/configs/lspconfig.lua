d270259b2616ca12e357b304a1c4c5b9d173b110
DraconicAsh <85758158+ConfidentlyAsh@users.noreply.github.com>
2023-09-01 18:51:17 -0500

    Add basic lsp support for neovim

---
+++ nvim/custom/configs/lspconfig.lua
@@ -0,0 +1,23 @@
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "clangd", "rust_analyzer"}
+
+for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+end
+
+require'lspconfig'.rust_analyzer.setup{
+    settings = {
+        ['rust-analyzer'] = {
+            check = {
+                command = "clippy",
+            }
+        }
+    }
+}
--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -1,31 +1,49 @@
 local plugins = {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {
-            "bash",
-            "rust",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "bash",
+                "rust",
 
-            -- webdev
-            "css",
-            "html",
-            "javascript",
+                -- webdev
+                "css",
+                "html",
+                "javascript",
 
-            -- data
-            "json",
-            "toml",
-            "yaml",
+                -- data
+                "json",
+                "toml",
+                "yaml",
 
-            -- git
-            "git_config",
-            "git_rebase",
-            "gitattributes",
-            "gitcommit",
-            "gitignore",
+                -- git
+                "git_config",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "gitignore",
 
-            -- defaults
-            "vim",
-            "lua"
-        }
+                -- defaults
+                "vim",
+                "lua"
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "lua-language-server",
+                "rust-analyzer",
+            },
+        },
     }
 }
 return plugins
